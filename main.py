64ca85fdf2a57ddd3dc8741eca47661ae0ba3b0c
Paloma Cione <paloma_zankely@hotmail.com>
2019-12-13 17:53:02 -0300

    Adds DELTE /users/:userid and POST /users/:userId/urls

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, request, render_template, Response
 import dataset
+from algorithm import *
 app = Flask(__name__)
 
 db = dataset.connect('postgresql://postgres:postgres@localhost:5432/mydatabase')
 users = db['users']
-
+urls = db['urls']
+urls.create_column('url', db.types.text)
+urls.create_column('hits', db.types.integer)
 @app.route("/")
 def main_page():
     return {"hello": "world"}
@@ -20,5 +23,31 @@ def add_user():
         result = {'id': id}
         return jsonify(result), 201
 
+@app.route("/users/<userId>", methods=["DELETE"])
+def delete(userId):
+    if users.find_one(username = userId):
+        users.delete(username = userId)
+        result = {'':''}
+        return jsonify(result), 200
+    else:
+        result = {'':''}
+        return jsonify(result), 404
+
+@app.route("/users/<userId>/urls", methods=["POST"])
+def post(userId):
+    if not users.find_one(username = userId):
+        result = {'':''}
+        return jsonify(result), 404
+    else:
+        long_url = request.json["url"]
+        if urls.find_one(url = long_url):
+            result = {'url': long_url}
+            return jsonify(result), 409
+        else:
+            short_url = ''
+            [n] = db.query("INSERT INTO urls (url,hits) VALUES (:long_url, 0) RETURNING id", long_url=long_url)
+            short_url = encode(n['id'])
+            result = {'id':"{}".format(n['id']), "hits": 0, "url": long_url, "shortUrl": "http://localhost:5000/{}".format(short_url)}
+            return jsonify(result), 201
 if __name__ == '__main__':
     app.run(debug=True)

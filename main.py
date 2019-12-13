d61af51e52a87a279a00e5280dcff26ab7a32b2a
Paloma Cione <paloma_zankely@hotmail.com>
2019-12-13 18:44:22 -0300

    removes conflict, users can have short urls mapping to the same long one

--- main.py
+++ main.py
@@ -40,14 +40,20 @@ def post(userId):
         return jsonify(result), 404
     else:
         long_url = request.json["url"]
-        if urls.find_one(url = long_url):
-            result = {'url': long_url}
-            return jsonify(result), 409
-        else:
-            short_url = ''
-            [n] = db.query("INSERT INTO urls (url,hits) VALUES (:long_url, 0) RETURNING id", long_url=long_url)
-            short_url = encode(n['id'])
-            result = {'id':"{}".format(n['id']), "hits": 0, "url": long_url, "shortUrl": "http://localhost:5000/{}".format(short_url)}
-            return jsonify(result), 201
+        short_url = ''
+        [n] = db.query("INSERT INTO urls (url,hits) VALUES (:long_url, 0) RETURNING id", long_url=long_url)
+        short_url = encode(n['id'])
+        data = dict(id = n['id'], shortUrl = short_url)
+        urls.upsert(data, ['id'])
+        result = {'id':"{}".format(n['id']), "hits": 0, "url": long_url, "shortUrl": "http://localhost:5000/urls/{}".format(short_url)}
+        return jsonify(result), 201
+
+@app.route("/urls/<id>", methods=["GET"])
+def get_redirect(id):
+        if not urls.find_one(id = str(id)):
+            result = {'':''}
+            return jsonify(result), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
